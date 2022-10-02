e4d21d3a119f9f0fa22bed200b6c582be218d0e2
Dahaka_Develop <typeboolean46@gmail.com>
2022-10-02 20:14:20 +0300

    commiting output files. All tasks are completed.

---
+++ Additionals.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop2
+{
+    public partial class BoolVector
+    {
+        public override string ToString()
+        {
+            return "Vector: " + "ID" + ID + "Name: " + name + "lenght: " + length + " Value: " + Value + " weight:  " + weight;
+        }
+        public override bool Equals(object obj)
+        {
+            // если параметр метода представляет тип BoolVector
+            // то возвращаем true, если фамилии совпадают
+            if (obj is BoolVector boolVector) return Value == boolVector.Value;
+            return false;
+        }
+        // вместе с методом Equals следует реализовать метод GetHashCode
+        public override int GetHashCode() => Value.GetHashCode();
+    }
+}
